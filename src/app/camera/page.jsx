02d5eca1f027e55990d4c889aeeb84dae20925b1
Yosef Awad <yosef.awad.1@gmail.com>
2024-05-01 01:27:18 +0300

    camera capture complete

--- src/app/camera/page.jsx
+++ src/app/camera/page.jsx
@@ -1,36 +1,83 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "@mui/material";
 import { Camera as CameraIcon } from "@mui/icons-material";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 import CameraView from "../../components/CameraView";
 import style from "./page.module.css";
 
 export default function Camera() {
   const [openCamera, setOpenCamera] = useState(false);
+  const canvasRef = useRef(null);
+  const [imgExist, setImgExist] = useState(false);
+
+  const openCloseCamera = () => {
+    setOpenCamera(!openCamera);
+  };
+
+  const setPicture = (video) => {
+    const canvas = canvasRef.current;
+    if (video && canvas) {
+      const context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      setImgExist(true);
+    }
+  };
   return (
     <div className={style.container}>
       <h1>Camera</h1>
       {/* Conditionally render CameraView */}
-      {openCamera && <CameraView />}
-
-      <div className={style.listContainer}>
-        <ol>
-          <li>take a picture</li>
-          <li>upload the picture</li>
-          <li>review the ideas</li>
-          <li>upcycle your trash</li>
-          <li>save the environment</li>
-        </ol>
-      </div>
-      <div className={style.btnContainer}>
-        <Button
-          variant="contained"
-          startIcon={<CameraIcon />}
-          onClick={() => setOpenCamera(!openCamera)}
-        >
-          {openCamera ? "Close Camera" : "Open Camera"}
-        </Button>
-      </div>
+      {openCamera && (
+        <CameraView setPicture={setPicture} setCancel={openCloseCamera} />
+      )}
+      <canvas
+        ref={canvasRef}
+        style={{ display: `${imgExist ? "block" : "none"}` }}
+      ></canvas>
+      {imgExist ? (
+        <>
+          <div className={style.uploadBtnContainer}>
+            <Button
+              variant="contained"
+              startIcon={<CameraIcon />}
+              onClick={() => openCloseCamera()}
+            >
+              retake
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<FileUploadIcon />}
+              onClick={() => {
+                // TODO: upload image to server
+              }}
+            >
+              Upload
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={style.listContainer}>
+            <ol>
+              <li>take a picture</li>
+              <li>upload the picture</li>
+              <li>review the ideas</li>
+              <li>upcycle your trash</li>
+              <li>save the environment</li>
+            </ol>
+          </div>
+          <div className={style.btnContainer}>
+            <Button
+              variant="contained"
+              startIcon={<CameraIcon />}
+              onClick={() => openCloseCamera()}
+            >
+              Open Camera
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/CameraView.jsx
+++ src/components/CameraView.jsx
@@ -1,17 +1,13 @@
 import React, { useRef, useEffect, useState } from "react";
-
-export default function CameraView() {
+import style from "./CameraView.module.css";
+export default function CameraView({ setPicture, setCancel }) {
   const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-  const [imgExists, setImgExists] = useState(false);
 
   useEffect(() => {
-    // Get access to the camera
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
         .getUserMedia({ video: { audio: false, facingMode: "environment" } })
         .then((stream) => {
-          // Set the video source to the video stream from the camera
           if (videoRef.current) videoRef.current.srcObject = stream;
         })
         .catch(function (error) {
@@ -21,33 +17,40 @@ export default function CameraView() {
   }, []);
 
   const captureImage = () => {
-    console.log("capturing image");
     const video = videoRef.current;
-    const canvas = canvasRef.current;
-    if (video && canvas) {
-      const context = canvas.getContext("2d");
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-      setImgExists(true);
-      // You can also save this image or process it further here
+    if (video && setPicture) {
+      setPicture(video);
+      if (setCancel) {
+        setCancel();
+      }
     }
   };
 
+  const cancel = () => {
+    if (setCancel) {
+      setCancel();
+    }
+  };
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
+    <div className={style.camera}>
       <video
+        className={style.video}
         ref={videoRef}
         autoPlay
         playsInline
         muted
         style={{ width: "100%" }}
       ></video>
-      <button onClick={captureImage}>Take Picture</button>
-      <canvas
-        ref={canvasRef}
-        style={{ display: `${imgExists ? "block" : "none"}` }}
-      ></canvas>
+      <div className={style.btnDiv}>
+        <button className={style.takePictureBtn} onClick={captureImage}>
+          <span className={style.innerCircle}></span>
+        </button>
+      </div>
+      <div className={style.cancelBtnDiv}>
+        <button className={style.cancelBtn} onClick={cancel}>
+          cancel
+        </button>
+      </div>
     </div>
   );
 }
